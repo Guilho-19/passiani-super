5cc4e417fa939e5be5b5bbd77624c39019de9a2b
Guilho-19 <guilhermepalaronunessilva@gmail.com>
2026-09-09 14:48:12 -0300

    Analytics fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
 // Arquivo principal de scripts
 console.log("Sistema Gerente Passiani iniciado com sucesso.");
 
-import { Analytics } from '@vercel/analytics/react';
-
-export default function App() {
-  return (
-    <div>
-      <h1>Meu Site</h1>
-      <Analytics /> {/* Adicione esta linha */}
-    </div>
-  );
-}
